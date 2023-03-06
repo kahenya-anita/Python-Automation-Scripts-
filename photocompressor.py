7cfae3217a2c8798836903ac325b5ed1de6619e9
kahenya-anita <anitakahenya1@gmail.com>
2023-03-06 14:50:58 +0300

    Add photo compressor script

---
+++ photocompressor.py
@@ -0,0 +1,8 @@
+# decrease the size of a picture by compressing it – while still keeping its quality.
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+fl = askopenfilenames()
+img = Image.open(fl[0])
+img.save("output.jpg", "JPEG", optimize=True, quality=10)
